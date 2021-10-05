9e948496aada9eb30b213e25098744ec0b0dce2a
Shakib053 <75584294+Shakib053@users.noreply.github.com>
2021-10-05 21:50:56 +0600

    final

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -5,6 +5,7 @@ const NotFound = () => {
         <div>
             <p>404 Error</p>
             <h3>Page Not Found</h3>
+            <h4>The link you are trying to access is either expired or broken</h4>
         </div>
     );
 };
